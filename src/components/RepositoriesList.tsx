82a36e77ec3da3813ddf684ce04d4098ae9a9cdf
juanmiguel431 <juanmiguel431@gmail.com>
2023-01-07 14:35:55 -0500

    useAppSelector refactored.

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
 import { useActions } from '../hooks/useActions';
-import { useSelector } from '../hooks/useTypedSelector';
+import { useAppSelector } from '../hooks/useAppSelector';
 
 export const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
-  const { loading, error, data } = useSelector(state => state.repositories);
+  const { loading, error, data } = useAppSelector(state => state.repositories);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
--- src/hooks/useTypedSelector.ts
+++
@@ -1,4 +0,0 @@
-import { useSelector as _useSelector, TypedUseSelectorHook } from 'react-redux';
-import { RootState } from '../state';
-
-export const useSelector: TypedUseSelectorHook<RootState> = _useSelector;
